70172279eb3f856a30627ff7f825438e82a47ff6
chubu-recycle5700106 <135834396+chubu-recycle5700106@users.noreply.github.com>
2023-06-14 20:26:34 +0900

    Update 1.1.py
    1.1修正

--- 1.1.py
+++ 1.1.py
@@ -3,14 +3,12 @@
 # int型のリストdataを作成し、値を1,3,5,7で初期化
 list = [1,3,5,7]
 
-i = 0
+
 
 for i in list:
     print(i*i)
 
 #問1-2
-j = 0
-for j in range(8):
-    if j %2 == 1:
-     a = j**2
-     print(a)
+
+for j in range(1,8,2):
+    print(j*j)
